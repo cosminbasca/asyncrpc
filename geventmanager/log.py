a4b732e65a53dd5a413fbbdca4df9952f8d03ec3
Cosmin Basca <cosmin.basca@gmail.com>
2014-06-18 17:01:00 +0000

    geventmanager version 0.1.2
    - logging module added

--- geventmanager/log.py
+++ geventmanager/log.py
@@ -4,6 +4,7 @@ __all__ = ['get_logger']
 
 __author__ = 'basca'
 
+# see more @ http://victorlin.me/posts/2012/08/26/good-logging-practice-in-python
 logging.config.fileConfig('logging.ini')
 
 def get_logger(name):
