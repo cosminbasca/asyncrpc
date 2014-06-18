7a4e17ebd26e0eed50ff83b759b974254a332596
Cosmin Basca <cosmin.basca@gmail.com>
2014-06-18 08:19:32 +0000

    version 0.1.12
    - packb and unpackb aliases in

---
+++ _bench.py
@@ -0,0 +1,24 @@
+from time import time
+import numpy as np
+from pandas import DataFrame
+
+__author__ = 'basca'
+
+def main():
+    data = [range(100) for i in xrange(10000) ]
+    df = DataFrame(data)
+
+    t0 = time()
+    v = df.values.tolist()
+    print '[numpy tolist        ] took {0} miliseconds'.format(time()-t0)
+
+    _itertuples = df.itertuples
+    t0 = time()
+    v = [r for r in _itertuples(index=False)]
+    print '[list comprehension  ] took {0} miliseconds'.format(time()-t0)
+
+
+if __name__ == '__main__':
+    main()
+
+
