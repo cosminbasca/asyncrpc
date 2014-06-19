0f9272f5ba7f509e0c7205a868e2bf3223bbeee7
Cosmin Basca <cosmin.basca@gmail.com>
2014-06-19 07:20:00 +0000

    geventmanager version 0.1.3
    - exceptions in

--- _bench.py
+++ _bench.py
@@ -5,17 +5,17 @@ from pandas import DataFrame
 __author__ = 'basca'
 
 def main():
-    data = [range(100) for i in xrange(10000) ]
+    data = [range(100) for i in xrange(100000) ]
     df = DataFrame(data)
 
     t0 = time()
     v = df.values.tolist()
-    print '[numpy tolist        ] took {0} miliseconds'.format(time()-t0)
+    print '[numpy tolist        ] took {0} seconds'.format(time()-t0)
 
     _itertuples = df.itertuples
     t0 = time()
     v = [r for r in _itertuples(index=False)]
-    print '[list comprehension  ] took {0} miliseconds'.format(time()-t0)
+    print '[list comprehension  ] took {0} seconds'.format(time()-t0)
 
 
 if __name__ == '__main__':
