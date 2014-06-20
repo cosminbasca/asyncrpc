9d6185d6e932ad0ab05c380e5bba7b24f22e4a14
Cosmin Basca <cosmin.basca@gmail.com>
2014-06-20 09:20:37 +0000

    geventmanager version 0.1.3
    - exceptions in

--- _bench.py
+++ _bench.py
@@ -1,11 +1,14 @@
+import inspect
 from time import time
 import numpy as np
 from pandas import DataFrame
+import numpy as np
+import numpy.random as rnd
 
 __author__ = 'basca'
 
 def main():
-    data = [range(100) for i in xrange(100000) ]
+    data = [range(100) for i in xrange(1000) ]
     df = DataFrame(data)
 
     t0 = time()
@@ -17,8 +20,53 @@ def main():
     v = [r for r in _itertuples(index=False)]
     print '[list comprehension  ] took {0} seconds'.format(time()-t0)
 
+    class TestObj(object):
+        def m_a(self):
+            print 'm_a'
+
+        def m_b(self):
+                print 'm_b'
+
+        def m_c(self):
+                print 'm_c'
+
+        def m_d(self):
+                print 'm_d'
+
+        def m_e(self):
+                print 'm_e'
+
+        def m_f(self):
+                print 'm_f'
+
+        def m_g(self):
+                print 'm_g'
+
+    to = TestObj()
+    methods = inspect.getmembers(to, predicate=inspect.ismethod)
+    methods = {method_name: impl for method_name, impl in methods if not method_name.startswith('_')}
+
+    to_call = ['m_{0}'.format(l) for l in 'abcdefg']
+
+    calls = rnd.random_integers(0, len(to_call)-1, 100000)
+
+    t0 = time()
+    for i in calls:
+        func = methods[to_call[i]]
+    print '[dict                ] took {0} seconds'.format(time()-t0)
+
+    t0 = time()
+    for i in calls:
+        func = getattr(to, to_call[i])
+    print '[getattr             ] took {0} seconds'.format(time()-t0)
+
+
 
 if __name__ == '__main__':
     main()
+    # [numpy tolist        ] took 0.00174593925476 seconds      numpy tolist is 25x faster than list comprehension
+    # [list comprehension  ] took 0.0259149074554 seconds
+    # [dict                ] took 0.0167138576508 seconds       DICT 2x faster than getattr
+    # [getattr             ] took 0.028911113739 seconds
 
 
--- geventmanager/server.py
+++ geventmanager/server.py
@@ -9,6 +9,8 @@ from geventmanager.exceptions import current_error
 from geventmanager.log import get_logger
 from geventmanager.rpcsocket import InetRpcSocket, GeventRpcSocket, RpcSocket
 from threading import Thread, BoundedSemaphore
+import preforkserver as pfs
+from multiprocessing import cpu_count
 
 __author__ = 'basca'
 
@@ -28,6 +30,9 @@ _INIT_ROBJ = _command('init_robj')
 # base Rpc server api
 #
 # ----------------------------------------------------------------------------------------------------------------------
+class RpcHandler(object):
+    def __init__(self):
+
 class RpcServer(object):
     __metaclass__ = ABCMeta
 
@@ -47,7 +52,7 @@ class RpcServer(object):
         self._address = (host, port)
 
         methods = inspect.getmembers(self.rpc_handler, predicate=inspect.ismethod)
-        self._methods = set([method_name for method_name, impl in methods if not method_name.startswith('_')])
+        self._methods = {method_name: impl for method_name, impl in methods if not method_name.startswith('_')}
 
         self._log = get_logger(self.__class__.__name__)
 
@@ -97,10 +102,10 @@ class RpcServer(object):
                 self._log.debug('received SHUTDOWN')
                 self.shutdown()
                 result = True
-            elif name not in self._methods:
-                raise NameError('method "{0}" not found in handler object'.format(name))
             else:
-                func = getattr(self.rpc_handler, name)
+                func = self._methods.get(name, None)
+                if not func:
+                    raise NameError('method "{0}" not found in handler object'.format(name))
                 result = func(*args, **kwargs)
             error = None
         except Exception:
@@ -109,7 +114,6 @@ class RpcServer(object):
             self._log.error('[_handle_request] error, traceback: \n{0}'.format(traceback.format_exc()))
         response = dumps((result, error))
         sock.write(response)
-
 
 
 # ----------------------------------------------------------------------------------------------------------------------
@@ -166,7 +170,7 @@ class ThreadedRpcServer(RpcServer):
 # preforked RPC Server (backed by Inet sockets)
 #
 # ----------------------------------------------------------------------------------------------------------------------
-class RpcHandlerChild(preforkserver.BaseChild):
+class RpcHandlerChild(pfs.BaseChild):
     _methods = None
     rpc_handler = None
     shutdown = None
@@ -174,56 +178,40 @@ class RpcHandlerChild(preforkserver.BaseChild):
     # def initialize(self):
     # def handle_terminate(signum, frame):
     # logger.info('SIGTERM received for child process [%s]'%(os.getpid()))
-    #         sys.exit()
-    #     signal.signal(signal.SIGTERM, handle_terminate)
+    # sys.exit()
+    # signal.signal(signal.SIGTERM, handle_terminate)
 
     def processRequest(self):
         sock = InetRpcSocket(self.conn)
         self._handle_request(sock, self.shutdown, self._methods, self.rpc_handler)
 
 
-class PreforkRPCManager(preforkserver.Manager):
+class PreforkRPCManager(pfs.Manager):
     def __init__(self, childClass,
-                 maxServers=20,
-                 minServers=5,
-                 minSpareServers=2,
-                 maxSpareServers=10,
-                 maxRequests=0,
-                 bindIp='127.0.0.1',
+                 max_servers=20,
+                 min_servers=5,
+                 min_spare_servers=2,
+                 max_spare_servers=10,
+                 max_requests=0,
+                 bind_ip='127.0.0.1',
                  port=10000,
-                 proto='tcp',
+                 protocol='tcp',
                  listen=5):
         super(PreforkRPCManager, self).__init__(childClass,
-                                                maxServers=maxServers,
-                                                minServers=minServers,
-                                                minSpareServers=minSpareServers,
-                                                maxSpareServers=maxSpareServers,
-                                                maxRequests=maxRequests,
-                                                bindIp=bindIp,
+                                                max_servers=max_servers,
+                                                min_servers=min_servers,
+                                                min_spare_servers=min_spare_servers,
+                                                max_spare_servers=max_spare_servers,
+                                                max_requests=max_requests,
+                                                bind_ip=bind_ip,
                                                 port=port,
-                                                proto=proto,
+                                                protocol=protocol,
                                                 listen=listen)
-        # bind at init... (so we can read address from it ... )
-        super(PreforkRPCManager, self).preBind()
-        super(PreforkRPCManager, self)._bind()
-        super(PreforkRPCManager, self).postBind()
-
-    def _bind(self):
-        # TODO: overriden so that there is no binding during call to run! (a hack until the API changes...)
-        pass
-
-    def _signalSetup(self):
-        # TODO: overriden so that there is no signal setup during call to run! (a hack until the API changes...)
-        pass
 
 
 class PreforkedRpcServer(RpcServer):
-    def __init__(self, host, rpc_handler, backlog=DEFAULT_SOCK_BACKLOG,
-                 maxServers=cpu_count() * 2,
-                 minServers=cpu_count(),
-                 minSpareServers=cpu_count(),
-                 maxSpareServers=cpu_count(),
-                 maxRequests=0):
+    def __init__(self, host, rpc_handler, backlog=64, max_servers=cpu_count() * 2, min_servers=cpu_count(),
+                 min_spare_servers=cpu_count(), max_spare_servers=cpu_count(), max_requests=0):
         super(PreforkedRpcServer, self).__init__(host, rpc_handler)
 
         self._Child = RpcHandlerChild
@@ -231,29 +219,24 @@ class PreforkedRpcServer(RpcServer):
         self._Child.rpc_handler = self.rpc_handler
         self._Child.shutdown = self.shutdown
 
-        self._manager = PreforkRPCManager(self._Child,
-                                          maxServers=maxServers,
-                                          minServers=minServers,
-                                          minSpareServers=minSpareServers,
-                                          maxSpareServers=maxSpareServers,
-                                          maxRequests=maxRequests,
-                                          bindIp=self._host_name,
-                                          port=self._host_port,
-                                          proto='tcp',
-                                          listen=backlog)
-        self._address = self.get_address(self._manager.accSock)
+        self._manager = pfs.Manager(self._Child, max_servers=max_servers, min_servers=min_servers,
+                                    min_spare_servers=min_spare_servers, max_spare_servers=max_spare_servers,
+                                    max_requests=max_requests, bind_ip=self.host, port=self.port, protocol='tcp',
+                                    listen=backlog)
 
-    address = property(fget=lambda self: self._address)
+    @property
+    def bound_address(self):
+        return self._manager.bound_address
 
     def close(self):
-        self.info('closing ... ')
+        self._log.info('closing ... ')
         if hasattr(self.rpc_handler, 'close'):
             self.rpc_handler.close()
         self._manager.close()
         # self.info('signaling children')
         # self.signal_children()
-        self.info('exiting ... ')
+        self._log.info('exit')
 
     def run(self):
-        self.info('starting')
+        self._log.info('starting ... ')
         self._manager.run()
