0ce9b9dceb532ee9fc7dd472d0b8d013fc82e65a
Cosmin Basca <cosmin.basca@gmail.com>
2014-06-27 14:30:57 +0000

    geventmanager version 0.1.14
    - without the immutable preforked component ...

--- geventmanager/manager.py
+++ geventmanager/manager.py
@@ -69,31 +69,3 @@ class GeventManager(object):
     @property
     def bound_address(self):
         return self._runner.bound_address
-
-
-        # class PreforkedManager(GeventManager):
-        # @classmethod
-        #     def register(cls, instance, **kwargs):
-        #         if '_registry' not in cls.__dict__:
-        #             cls._registry = cls._registry.copy()
-        #
-        #         instance_id = hash(instance)
-        #         cls._registry[instance_id] = instance
-        #
-        #         return instance_id
-        #
-        #     @property
-        #     def _server_class(self):
-        #         return PreforkedRpcServer
-        #
-        #     def get_instance(self, instance_id):
-        #         if self._async:
-        #             if self._async_pooled:
-        #                 proxy = GeventPooledProxy(instance_id, self._bound_address, concurrency=self._pool_concurrency)
-        #             else:
-        #                 proxy = GeventProxy(instance_id, self._bound_address)
-        #         else:
-        #             proxy = InetProxy(instance_id, self._bound_address)
-        #         self._log.debug(
-        #             'created proxy "{0}" for instance id={1}'.format(type(proxy), instance_id))
-        #         return proxy
