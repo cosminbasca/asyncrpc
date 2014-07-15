ed9f0124808d5c311e9fc9cc23b59527a9e827c7
Cosmin Basca <cosmin.basca@gmail.com>
2014-07-15 15:18:11 +0000

    geventmanager version 0.2.10
    - still with problems ...

--- geventmanager/httprpc/server.py
+++ geventmanager/httprpc/server.py
@@ -4,27 +4,25 @@ import inspect
 
 __author__ = 'basca'
 
-class WSGIRpcApplication(object):
+class HttpRpcHandler(object):
     pass
 
-class WSGIRpcServer(object):
-    __metaclass__ = ABCMeta
+class AsyncHttpRpcHandler(object):
+    pass
 
-    def __init__(self, handler):
-        self._handler = handler
-        self._log = get_logger(self.__class__.__name__)
+def get_routes(handler):
+    if handler is None:
+        raise ValueError('handler cannot be None')
 
-        methods = inspect.getmembers(self._handler, predicate=inspect.ismethod)
-        methods = [(method_name, impl) for method_name, impl in methods if not method_name.startswith('_')]
+    methods = [(method_name, impl) for method_name, impl in inspect.getmembers(handler, predicate=inspect.ismethod)
+               if not method_name.startswith('_') and getattr(impl, 'exposed', False) == True]
 
-        def _handler(method_name, impl):
-            TornadoHandlerClass = AsyncAvaTornadoRequestHandler if getattr(impl, 'async', False)  \
-                else AvaTornadoRequestHandler
-            ClassName = ('%s_AsyncTornadoHandler' if hasattr(impl,
-                                                             'async_rpc') else '%s_TornadoHandler') % method_name.upper()
-            return type(
-                ClassName, (TornadoHandlerClass,), {'do': getattr(self.rpc_handler, method_name), }
-            )
+    def route_handler(method_name, impl):
+        HandlerClass = AsyncHttpRpcHandler if getattr(impl, 'async', False) else HttpRpcHandler
+        class_name = '{0}_{1}'.format(HandlerClass.__name__, method_name.upper())
+        return type(class_name, (HandlerClass,), {'do': getattr(handler, method_name), })
 
-        handlers = [(r'/%s' % method_name, _handler(method_name, impl)) for method_name, impl in methods]
+
+    return [(r'/%s' % method_name, route_handler(method_name, impl)) for method_name, impl in methods]
+
 
