e67cd504abea31583cd1927795e83828b78a0ce0
Cosmin Basca <cosmin.basca@gmail.com>
2014-07-19 10:47:17 +0000

    asyncrpc version 0.3.10
    - wsgi app: registry viewer on /
    - tornado multi-process still fails

--- __deprecated__/__init__.py
+++
@@ -1,8 +0,0 @@
-__author__ = 'basca'
-
-from rpcsocket import GeventRpcSocket, InetRpcSocket, RpcSocket
-from proxy import InetProxy, GeventProxy, GeventPooledProxy, Dispatcher, Proxy, dispatch
-from server import RpcServer, ThreadedRpcServer, PreforkedRpcServer
-from manager import GeventManager, PreforkedSingletonManager
-from log import get_logger, set_level
-from exceptions import *
