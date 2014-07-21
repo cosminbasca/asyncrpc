6bdcb2616f4920e4d2845174c2c7dbeaa5aa7b76
Cosmin Basca <cosmin.basca@gmail.com>
2014-07-21 18:50:36 +0000

    version 0.3.23
    - profile server  in

--- _bench.py
+++ _bench.py
@@ -1,80 +1,20 @@
-import inspect
-from time import time, sleep
-from pandas import DataFrame
-import numpy.random as rnd
-from geventmanager import set_level, get_logger
-from geventmanager.manager import GeventManager, PreforkedSingletonManager
-import gevent
+from time import time
+from asyncrpc import set_level, create
+from asyncrpc.manager import AsyncManager
 from gevent.pool import Pool
-from random import random
 from multiprocessing.pool import ThreadPool
+import numpy as np
 
 __author__ = 'basca'
 
 set_level('warning')
-# set_level('debug')
-# set_level('debug')
 
-def main():
-    data = [range(100) for i in xrange(1000)]
-    df = DataFrame(data)
-
-    t0 = time()
-    v = df.values.tolist()
-    print '[numpy tolist        ] took {0} seconds'.format(time() - t0)
-
-    _itertuples = df.itertuples
-    t0 = time()
-    v = [r for r in _itertuples(index=False)]
-    print '[list comprehension  ] took {0} seconds'.format(time() - t0)
-
-    class TestObj(object):
-        def m_a(self):
-            print 'm_a'
-
-        def m_b(self):
-            print 'm_b'
-
-        def m_c(self):
-            print 'm_c'
-
-        def m_d(self):
-            print 'm_d'
-
-        def m_e(self):
-            print 'm_e'
-
-        def m_f(self):
-            print 'm_f'
-
-        def m_g(self):
-            print 'm_g'
-
-    to = TestObj()
-    methods = inspect.getmembers(to, predicate=inspect.ismethod)
-    methods = {method_name: impl for method_name, impl in methods if not method_name.startswith('_')}
-
-    to_call = ['m_{0}'.format(l) for l in 'abcdefg']
-
-    calls = rnd.random_integers(0, len(to_call) - 1, 100000)
-
-    t0 = time()
-    for i in calls:
-        func = methods[to_call[i]]
-    print '[dict                ] took {0} seconds'.format(time() - t0)
-
-    t0 = time()
-    for i in calls:
-        func = getattr(to, to_call[i])
-    print '[getattr             ] took {0} seconds'.format(time() - t0)
-
-import numpy as np
 
 class MyClass(object):
     def __init__(self, counter=0, wait=False):
         self._c = counter
         self._w = wait
-        print 'with wait = ',True if self._w else False
+        print 'with wait = ', True if self._w else False
 
     def add(self, value=1):
         self._c += value
@@ -83,18 +23,16 @@ class MyClass(object):
         self._c -= value
 
     def current_counter(self):
-        # if self._w: sleep(random() * 0.8) # between 0 and .8 seconds
-        # if self._w: self._c = sum([i ** 2 for i in xrange(int(random() * 100000))])  # a computation ...
         if self._w: self._c = np.exp(np.arange(1000000)).sum()
         return self._c
 
 
-def bench_gevent_man(async=False, pooled=False, wait=False):
-    class MyManager(GeventManager):
+def bench(async=False, wait=False):
+    class MyManager(AsyncManager):
         pass
 
     MyManager.register("MyClass", MyClass)
-    manager = MyManager(async=async, async_pooled=pooled)
+    manager = MyManager(async=async)
     manager.start(threads=512)
 
     my1 = manager.MyClass(counter=10, wait=wait)
@@ -118,64 +56,24 @@ def bench_gevent_man(async=False, pooled=False, wait=False):
     print 'done'
 
 
-
-# def bench_prefork_man(async=False, pooled=False):
-#     my_instance = MyClass(counter=10)
-#     manager = PreforkedSingletonManager(my_instance, slots=['current_counter'], async=async, async_pooled=pooled)
-#     manager.start()
-#
-#     manager.debug()
-#
-#     my_instance = manager.proxy
-#     calls = 10000
-#     t0 = time()
-#     for i in xrange(calls):
-#         my_instance.current_counter()
-#     t1 = time() - t0
-#     ncalls = long(float(calls) / float(t1))
-#     print 'DID: {0} calls / second'.format(ncalls)
-#
-#     del manager
-#     print 'done'
-#
-#
-# def bench_old_geventman(async=False, pooled=False):
-#     from geventmanager.__deprecated__.sockrpc import GeventManager as _GeventManager
-#
-#     manager = _GeventManager(MyClass, handler_args=(), handler_kwargs={'counter': 10},
-#                              host=None, logger=get_logger(_GeventManager.__name__),
-#                              prefork=False, async=async, pooled=pooled, gevent_patch=False, concurrency=32)
-#     manager.start()
-#
-#     my1 = manager.proxy
-#
-#     calls = 10000
-#     t0 = time()
-#     for i in xrange(calls):
-#         my1.current_counter()
-#     t1 = time() - t0
-#     ncalls = long(float(calls) / float(t1))
-#     print 'DID: {0} calls / second'.format(ncalls)
-#
-#     del manager
-#     print 'done'
+def _run_client(async=False, wait=True):
+    my1 = create(('127.0.0.1', 8080), 'MyClass', None, async, counter=100, wait=wait)
+    calls = 10000
+    concurrent = 512
+    t0 = time()
+    if async:
+        pool = Pool(concurrent)
+        [pool.apply_async(my1.current_counter) for i in xrange(calls)]
+        pool.join()
+    else:
+        pool = ThreadPool(concurrent)
+        [pool.apply_async(my1.current_counter) for i in xrange(calls)]
+        pool.close()
+        pool.join()
+    t1 = time() - t0
+    ncalls = long(float(calls) / float(t1))
+    print 'DID: {0} calls / second, total calls: {1}'.format(ncalls, calls)
 
 
 if __name__ == '__main__':
-    # bench_gevent_man(async=False, pooled=False)
-
-    # bench_gevent_man(async=True, pooled=False, wait=False)
-    # DID: 1852 calls / second, total 10000 results
-
-    bench_gevent_man(async=True, pooled=False, wait=True)
-    # DID: 1717 calls / second, total calls: 10000,             256 connections, wait
-    # DID: 152 calls / second, total calls: 10000               256 connections, computation
-
-    # bench_gevent_man(async=False, pooled=False, wait=True)
-    # DID: 805 calls / second, total calls: 10000               256 connections, wait
-    # DID: 122 calls / second, total calls: 10000               256 connections, computation
-
-    # :( manager server gets saturated ... :|
-
-
-
+    _run_client(False, True)
--- _profile.py
+++ _profile.py
@@ -1,49 +1,30 @@
 import pstats
 import cProfile
-from time import sleep
 import _bench
+from asyncrpc import CherrypyRpcServer
 
 __author__ = 'basca'
 
 
-def profile(prefork=False, async=False, pooled=False, wait=1):
-    call = 'bench_prefork_man' if prefork else 'bench_gevent_man'
-    stats_name = "profile_{0}_async_{1}_pooled_{2}.prof".format('pfork' if prefork else 'gevent',
-                                                                'T' if async else 'F',
-                                                                'T' if pooled else 'F')
-    cProfile.runctx("_bench.{0}(async={1}, pooled={2}, wait={3})".format(call, async, pooled, wait), globals(), locals(),
-                    stats_name)
+def profile_client(async=False, wait=1):
+    stats_name = "profile_async_{0}.prof".format('T' if async else 'F')
+    cProfile.runctx("_bench.bench(async={0}, wait={1})".format(async, wait), globals(), locals(), stats_name)
     s = pstats.Stats(stats_name)
     print '------------------------------------------------------------------------------------------------------------'
     s.strip_dirs().sort_stats("time").print_stats()
 
+def _start_server():
+    registry = {'MyClass': _bench.MyClass}
+    cpsrv = CherrypyRpcServer(('127.0.0.1', 8080), registry)
+    cpsrv.start()
 
-def _profile(async=False, pooled=False, wait=1):
-    stats_name = "profile_{0}_async_{1}_pooled_{2}.prof".format('old',
-                                                                'T' if async else 'F',
-                                                                'T' if pooled else 'F')
-    cProfile.runctx("_bench.{0}(async={1}, pooled={2})".format('bench_old_geventman', async, pooled), globals(),
-                    locals(), stats_name)
-    s = pstats.Stats(stats_name)
-    print '------------------------------------------------------------------------------------------------------------'
-    s.strip_dirs().sort_stats("time").print_stats()
-    sleep(wait)
-
+def profile_server():
+    stats_name = "profile_server.prof"
+    cProfile.runctx("_start_server()", globals(), locals(), stats_name)
 
 if __name__ == '__main__':
-    # _profile(async=False, pooled=False)
+    pass
+    # profile_client(async=False, wait=True)
+    # profile_client(async=True, wait=True)
 
-    # profile(prefork=False, async=False, pooled=False, wait=False)
-    # nowait            DID: 464 calls / second, total 100000 results
-    # profile(prefork=False, async=False, pooled=False, wait=True)
-    # randwait(0,.8)
-
-    # profile(prefork=False, async=True, pooled=False, wait=False)
-    # nowait
-    profile(prefork=False, async=True, pooled=False, wait=True)
-    # randwait(0,.8)
-
-    # profile(prefork=False, async=True, pooled=True)
-    # profile(prefork=True, async=False, pooled=False)
-    # profile(prefork=True, async=True, pooled=False)
-    # profile(prefork=True, async=True, pooled=True)
+    profile_server()
