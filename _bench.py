fcadec3f85138b524bc5883956907b98a0b3c9ed
Cosmin Basca <cosmin.basca@gmail.com>
2014-07-28 20:53:37 +0000

    version 0.4.4
    - tornado specific async rpc is in (not tested)

--- _bench.py
+++ _bench.py
@@ -1,53 +1,79 @@
-from time import time
+from multiprocessing.managers import BaseManager
+from time import time, sleep
 from asyncrpc import set_level, create
 from asyncrpc.manager import AsyncManager
 from gevent.pool import Pool
 from multiprocessing.pool import ThreadPool
 import numpy as np
+from pandas import DataFrame
+import random
 
 __author__ = 'basca'
 
 set_level('warning')
 
 
-class MyClass(object):
-    def __init__(self, counter=0, wait=False):
-        self._c = counter
-        self._w = wait
-        print 'with wait = ', True if self._w else False
+class DataClass(object):
+    def __init__(self, rows=100000, columns=10):
+        self._columns = list(xrange(columns))
+        self._index = list(xrange(rows))
+        self._dframe = DataFrame(data=[[random.randint(0, 100000) for j in xrange(columns)] for i in xrange(rows)],
+                                 index=self._index,
+                                 columns=self._columns)
 
-    def add(self, value=1):
-        self._c += value
-
-    def dec(self, value=1):
-        self._c -= value
-
-    def current_counter(self):
-        if self._w: self._c = np.exp(np.arange(1000000)).sum()
-        return self._c
+    def get_table(self):
+        return self._dframe
 
 
-def bench(async=False, wait=False):
+
+def bench(async=False):
     class MyManager(AsyncManager):
         pass
 
-    MyManager.register("MyClass", MyClass)
+    MyManager.register("DataClass", DataClass)
     manager = MyManager(async=async)
-    manager.start(threads=512)
+    manager.start()
 
-    my1 = manager.MyClass(counter=10, wait=wait)
-    calls = 10000
-    concurrent = 512
+    my1 = manager.DataClass()
+    calls = 1000
+    concurrent = 128
     t0 = time()
     if async:
         pool = Pool(concurrent)
-        [pool.apply_async(my1.current_counter) for i in xrange(calls)]
+        [pool.apply_async(my1.get_table) for i in xrange(calls)]
         pool.join()
     else:
         pool = ThreadPool(concurrent)
-        [pool.apply_async(my1.current_counter) for i in xrange(calls)]
+        res = [pool.apply_async(my1.get_table) for i in xrange(calls)]
         pool.close()
         pool.join()
+        print [r.get().sum().sum() for r in res]
+    t1 = time() - t0
+    ncalls = long(float(calls) / float(t1))
+    print 'DID: {0} calls / second, total calls: {1}'.format(ncalls, calls)
+
+    del manager
+    print 'done'
+
+def bench_mpman():
+    class MyManager(BaseManager):
+        pass
+
+    MyManager.register("DataClass", DataClass)
+    manager = MyManager()
+    manager.start()
+
+    my1 = manager.DataClass()
+    calls = 1000
+    concurrent = 128
+    t0 = time()
+    pool = ThreadPool(concurrent)
+    res = [pool.apply_async(my1.get_table) for i in xrange(calls)]
+    pool.close()
+    pool.join()
+
+    print [r.get().sum().sum() for r in res]
+
     t1 = time() - t0
     ncalls = long(float(calls) / float(t1))
     print 'DID: {0} calls / second, total calls: {1}'.format(ncalls, calls)
@@ -56,24 +82,11 @@ def bench(async=False, wait=False):
     print 'done'
 
 
-def _run_client(async=False, wait=True):
-    my1 = create(('127.0.0.1', 8080), 'MyClass', None, async, counter=100, wait=wait)
-    calls = 10000
-    concurrent = 512
-    t0 = time()
-    if async:
-        pool = Pool(concurrent)
-        [pool.apply_async(my1.current_counter) for i in xrange(calls)]
-        pool.join()
-    else:
-        pool = ThreadPool(concurrent)
-        [pool.apply_async(my1.current_counter) for i in xrange(calls)]
-        pool.close()
-        pool.join()
-    t1 = time() - t0
-    ncalls = long(float(calls) / float(t1))
-    print 'DID: {0} calls / second, total calls: {1}'.format(ncalls, calls)
 
 
 if __name__ == '__main__':
-    _run_client(False, True)
+    pass
+    # bench(async=False)
+    bench(async=True)
+    # sleep(5)
+    # bench_mpman()
