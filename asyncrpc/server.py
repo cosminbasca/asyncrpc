6f0faa0a48d97c042e08a4751e56ae6f4cec2159
Cosmin Basca <cosmin.basca@gmail.com>
2014-08-17 07:41:41 +0000

    0.5.9

--- asyncrpc/server.py
+++ asyncrpc/server.py
@@ -175,38 +175,3 @@ class TornadoWsgiRpcServer(WsgiRpcServer):
     def bound_address(self):
         return self._bound_address
 
-
-if __name__ == '__main__':
-    # set_level('critical')
-    import numpy as np
-
-    class MyClass(object):
-        def __init__(self, counter=0, wait=False):
-            self._c = counter
-            self._w = wait
-            self.some_data = [10, 20, '30', 'hehe']
-            # print 'with wait = ', True if self._w else False
-
-        @property
-        def a_property(self):
-            return {'un':1, 'doi':2}
-
-        def add(self, value=1):
-            self._c += value
-
-        def dec(self, value=1):
-            self._c -= value
-
-        def current_counter(self):
-            # if self._w: sleep(random() * 0.8) # between 0 and .8 seconds
-            # if self._w: self._c = sum([i ** 2 for i in xrange(int(random() * 100000))])  # a computation ...
-            if self._w: self._c = np.exp(np.arange(1000000)).sum()
-            return self._c
-
-    registry = {'MyClass': MyClass}
-    cpsrv = CherrypyWsgiRpcServer(('127.0.0.1', 8080), registry)
-    # cpsrv = CherrypyRpcServer(('127.0.0.1', 0), registry)
-    # cpsrv = TornadoRpcServer(('127.0.0.1', 8080), registry)
-    # cpsrv = TornadoRpcServer(('127.0.0.1', 8080), registry)
-    # print 'BOUND to PORT = {0}'.format(cpsrv.bound_address)
-    cpsrv.start()
