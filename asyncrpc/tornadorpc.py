7543865b6a767e4731a65647e5405abdcf6aa85f
Cosmin Basca <cosmin.basca@gmail.com>
2014-08-19 09:09:08 +0000

    asyncrpc version 0.6.7
    - nicer templates for the viewers

--- asyncrpc/tornadorpc.py
+++ asyncrpc/tornadorpc.py
@@ -239,7 +239,6 @@ class TornadoRpcServer(RpcServer):
         super(TornadoRpcServer, self).__init__(address, *args, **kwargs)
         settings = {'template_path': os.path.join(os.path.dirname(__file__), 'templates'),
                     'static_path': os.path.join(os.path.dirname(__file__), 'static'),
-                    'debug': debug,
                     # 'xsrf_cookies': True,
         }
         app = TornadoRpcApplication(instance, handlers=[
