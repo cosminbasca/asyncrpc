6ba3d2342cade0288ab8626d2d5a717895bb5876
Cosmin Basca <cosmin.basca@gmail.com>
2014-10-09 14:58:46 +0000

    asyncrpc version 0.10.9
    - tests pass

--- asyncrpc/__version__.py
+++ asyncrpc/__version__.py
@@ -1,4 +1,4 @@
 __author__ = 'basca'
 
-version = (0, 10, 7)
+version = (0, 10, 9)
 str_version = '.'.join(['%s' % v for v in version])
--- asyncrpc/util.py
+++ asyncrpc/util.py
@@ -1,18 +1,23 @@
 __author__ = 'basca'
 
+__types_list = (list, tuple)
+__types_str = (str, unicode)
+
 
 def format_address(address):
-    if isinstance(address, (tuple, list)):
-        host, port = address
-    elif isinstance(address, (str, unicode)):
+    if isinstance(address, __types_str):
         host, port = address.split(':')
         port = int(port)
+    elif isinstance(address, __types_list):
+        host, port = address
     else:
         raise ValueError('address, must be either a tuple/list or string of the name:port form')
     return host, port
 
 
 def format_addresses(address):
-    if isinstance(address, (tuple, list)) and isinstance(address[0], (list, tuple)):
-        return map(format_address, address)
+    if isinstance(address, __types_list):
+        size = len(address)
+        if (size >=2 and isinstance(address[1], __types_str)) or size == 1:
+            return map(format_address, address)
     return format_address(address)
